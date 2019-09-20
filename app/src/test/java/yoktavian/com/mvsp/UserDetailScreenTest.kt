cc6b045365ab9c8d815ef591cb4752c65f4ab0a8
Yuda Oktavian Krisendi <yoktavian17@gmail.com>
2019-09-20 21:28:19 +0700

    unit test

--- app/src/test/java/yoktavian/com/mvsp/UserDetailScreenTest.kt
+++ app/src/test/java/yoktavian/com/mvsp/UserDetailScreenTest.kt
@@ -14,33 +14,35 @@ import yoktavian.com.mvsp.screen.user.UserDetailScreen
  * Created by YudaOktavian on 17-Aug-2019
  */
 @RunWith(JUnit4::class)
-class UserDetailScreenTest : BaseUnitTest<UserDetailScreen, UserDetailScreen.State, UserDetailScreen.Presenter>() {
+class UserDetailScreenTest :
+    BaseUnitTest<UserDetailScreen, UserDetailScreen.State, UserDetailScreen.Presenter>() {
 
-    private val mockedUser = User("Tes", "Solo", 123)
+    private val mockedUser = spyk(User("Tes", "Solo", 123))
     private val mockedDefaultUser: User = mockk(relaxed = true)
+    private val userRepository = spyk(UserRepository())
 
     @Before
     fun init() {
         view = spyk()
         state = spyk()
-        presenter = spyk(UserDetailScreen.Presenter(state, view, UserRepository()))
+        presenter = spyk(UserDetailScreen.Presenter(state, view, userRepository))
     }
 
     @Test
     fun `when fetch user detail, render loading should be called and must getting data from repo`() {
         // given
         every { presenter.repository.getUserData(captureLambda()) } answers {
-            lambda<(User) -> Unit>().invoke(mockedDefaultUser)
+            lambda<(User) -> Unit>().invoke(mockedUser)
         }
         every { view.renderLoading() } returns Unit
         // when
         presenter.fetchUserDetail()
-        print(mockedDefaultUser.name)
-        print(mockedDefaultUser.address)
-        print(mockedDefaultUser.phone)
+        print(mockedUser.name)
+        print(mockedUser.address)
+        print(mockedUser.phone)
         // then
         with(state) {
-            Assert.assertEquals(userData, mockedDefaultUser)
+            Assert.assertEquals(userData, mockedUser)
             Assert.assertEquals(isLoading, false)
         }
         verify(exactly = 2) {
